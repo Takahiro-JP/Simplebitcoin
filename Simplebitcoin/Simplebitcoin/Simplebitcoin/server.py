35b1f75e790d8533eb4178c7ea6df64aa0c4419b
松本貴大 <takahiro@takahiro.local>
2018-11-12 22:09:12 +0900

    test2

--- Simplebitcoin/Simplebitcoin/Simplebitcoin/server.py
+++
@@ -1,9 +0,0 @@
-from concurrent.futures import ThreadPoolExecutor
-import socket
-import os
-
-def __handle_message(args_tuple):
-
-conn, addr, data_sum = args_tuple
-while True:
- data = conn.recv(1024)
